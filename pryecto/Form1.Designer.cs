4d982a503017b71367c82f030970e58fd670fe2e
Xbeats26 <eskelerd@gmail.com>
2025-07-27 17:48:46 +0200

    Base del proyecto

---
+++ pryecto/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace pryecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnenviar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtcancion = new System.Windows.Forms.TextBox();
+            this.listcanciones = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnenviar
+            // 
+            this.btnenviar.Location = new System.Drawing.Point(951, 428);
+            this.btnenviar.Name = "btnenviar";
+            this.btnenviar.Size = new System.Drawing.Size(197, 38);
+            this.btnenviar.TabIndex = 1;
+            this.btnenviar.Text = "enviar";
+            this.btnenviar.UseVisualStyleBackColor = true;
+            this.btnenviar.Click += new System.EventHandler(this.btnenviar_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(837, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(629, 41);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "No encuentras tu cancion?";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(856, 364);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Envianos su nombre para que aparezca en la siguiente fiesta";
+            // 
+            // txtcancion
+            // 
+            this.txtcancion.Location = new System.Drawing.Point(951, 402);
+            this.txtcancion.Name = "txtcancion";
+            this.txtcancion.Size = new System.Drawing.Size(302, 20);
+            this.txtcancion.TabIndex = 4;
+            // 
+            // listcanciones
+            // 
+            this.listcanciones.FormattingEnabled = true;
+            this.listcanciones.Location = new System.Drawing.Point(117, 128);
+            this.listcanciones.Name = "listcanciones";
+            this.listcanciones.Size = new System.Drawing.Size(120, 95);
+            this.listcanciones.TabIndex = 5;
+            this.listcanciones.SelectedIndexChanged += new System.EventHandler(this.listcanciones_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(629, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Escoge aqui tu cancion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1326, 590);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listcanciones);
+            this.Controls.Add(this.txtcancion);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnenviar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnenviar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtcancion;
+        private System.Windows.Forms.ListBox listcanciones;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ pryecto/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+
+namespace pryecto
+{
+    public partial class Form1 : Form
+    {
+
+        //creamos la ruta de donde se almacenaran todas las canciones
+        string ruta = "canciones.txt";
+        //creamos la ruta donde se pondra las canciones para pinchar
+        string admin = "admin.txt";
+      
+        public Form1()
+        {
+            InitializeComponent();
+
+                                                                //pasar el contenido del txt a una listbox
+
+            
+            //creamos un lector que nos permitira leer linea a linea poniendo el contenido que encuentre en la listbox
+         using (StreamReader lector = new StreamReader(ruta,true))
+            {
+                //creamos la variable que almacenara el contenido que encuentre lector
+                //creamos un bucle que almacenara lo que encuentre lector en contenido, leyendo todas las lineas escritas
+                string contenido;
+                while ((contenido = lector.ReadLine()) != null) 
+                {
+                listcanciones.Items.Add(contenido);
+                }
+            }
+        }
+
+
+
+        public void btnenviar_Click(object sender, EventArgs e)
+        {
+                                             //pasar el contenido de una textbox a un txt-pasar el contenido del txt a una listbox
+
+
+            //creamos un escritor que recogera el texto de la textbox y lo almacenara en el documento
+            using (StreamWriter escritor = new StreamWriter(ruta,true))
+            {
+                escritor.WriteLine(txtcancion.Text);
+            }
+            
+            //ponemos la textbox vacia
+            txtcancion.Text = "";
+         
+            //vaciamos la lista para que no se duplique el contenido
+            listcanciones.Items.Clear();
+           
+            //creamos la variable que almacenara el contenido que encuentre lector
+            //creamos un bucle que almacenara lo que encuentre lector en contenido, leyendo todas las lineas escritas
+            using (StreamReader lector = new StreamReader(ruta, true))
+            {
+                string contenido;
+                while ((contenido = lector.ReadLine()) != null)
+                {
+                    listcanciones.Items.Add(contenido);
+                }
+            }
+
+
+
+
+        }
+
+        private void listcanciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //obtenemos la cancion 
+            string cancionlista = listcanciones.Text;
+
+            //mostramos una messagebox que almacenara el resultado (yes-no) en resultado, preguntando si es correcta la cancion
+            DialogResult resultado = MessageBox.Show(
+            "quieres que suene "+ cancionlista+"?",
+            cancionlista,
+            MessageBoxButtons.YesNo,
+            MessageBoxIcon.Question
+        );
+
+
+            //escribimos la cancion en un txt a parte que lo podra ver el administrador del programa
+            if (resultado == DialogResult.Yes)
+            {
+                using (StreamWriter escritor = new StreamWriter(admin,true))
+                {
+                   escritor.WriteLine(cancionlista);
+                }
+
+                MessageBox.Show("Gracias, nos aseguramos de poner tu cancion");
+
+            }
+
+        }
+    }
+}
